072adb692ea7b7b2cd97deb9ffee186c8cefeb62
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2024-03-19 09:59:42 +0600

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
-import s from './Header.module.scss'
+import React, { FC, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../Assets/image/Ramilya/logo.png'
-import tel from '../../Assets/image/Ramilya/tel.png'
-import adres from '../../Assets/image/Ramilya/adres.png'
+import s from './Header.module.scss';
+import logo from '../../Assets/image/Ramilya/logo.png';
+import tel from '../../Assets/image/Ramilya/tel.png';
+import adres from '../../Assets/image/Ramilya/adres.png';
 
 const Header: FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className={s.header}>
             <div className='container'>
@@ -25,28 +31,35 @@ const Header: FC = () => {
                     <NavLink to={'/'}>
                         <img src={logo} alt="logo" />
                     </NavLink>
-                    <div className={s.navbar}>
-                        <NavLink to={'/'}>
-                            <h2 className={s.nav_title}>главная</h2>
-                        </NavLink>
-                        <NavLink to={'/about_us'}>
-                            <h2 className={s.nav_title}>о нас</h2>
-                        </NavLink>
-                        <NavLink to={'/our_works'}>
-                            <h2 className={s.nav_title}>наши работы</h2>
-                        </NavLink>
-                        <NavLink to={'/services'}>
-                            <h2 className={s.nav_title}>УСЛУГИ</h2>
-                        </NavLink>
-                        <a href="#partners"></a>
-                        <h2 className={s.nav_title}>партнеры</h2>
-                        <select className={s.select}>
-                            <option value="ru">ру</option>
-                            <option value="eng">eng</option>
-                        </select>
-                        <NavLink to={'/form'}>
-                            <button className={s.btn_nav}>онлайн-запрос</button>
-                        </NavLink>
+                    <div className={`${s.navbar} ${isOpen ? s.active : ''}`}>
+                        <div className={s.burger} onClick={toggleMenu}>
+                            <div className={s.line}></div>
+                            <div className={s.line}></div>
+                            <div className={s.line}></div>
+                        </div>
+                        <div className={s.menu}>
+                            <NavLink to={'/'} onClick={toggleMenu}>
+                                <h2 className={s.nav_title}>главная</h2>
+                            </NavLink>
+                            <NavLink to={'/about_us'} onClick={toggleMenu}>
+                                <h2 className={s.nav_title}>о нас</h2>
+                            </NavLink>
+                            <NavLink to={'/our_works'} onClick={toggleMenu}>
+                                <h2 className={s.nav_title}>наши работы</h2>
+                            </NavLink>
+                            <NavLink to={'/services'} onClick={toggleMenu}>
+                                <h2 className={s.nav_title}>УСЛУГИ</h2>
+                            </NavLink>
+                            <a href="#partners" onClick={toggleMenu}></a>
+                            <h2 className={s.nav_title}>партнеры</h2>
+                            <select className={s.select} onClick={toggleMenu}>
+                                <option value="ru">ру</option>
+                                <option value="eng">eng</option>
+                            </select>
+                            <NavLink to={'/form'} onClick={toggleMenu}>
+                                <button className={s.btn_nav}>онлайн-запрос</button>
+                            </NavLink>
+                        </div>
                     </div>
                 </div>
             </header>
